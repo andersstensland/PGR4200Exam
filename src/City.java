fc19411c1503a2483b2c21e7c76a3b4113d64a2c
Anders Stensland <43165915+andersstensland@users.noreply.github.com>
2024-04-09 11:21:19 +0200

    init

---
+++ src/City.java
@@ -0,0 +1,11 @@
+public class City {
+    String name;
+    public double latitude;
+
+    public City(String name, double latitude) {
+        this.name = name;
+        this.latitude = latitude;
+    }
+
+
+}
---
+++ src/MergeSort.java
@@ -0,0 +1,10 @@
+public class MergeSort {
+    public static int[] merge() {
+
+        return new int[0];
+    }
+
+    public static void mergeSort() {
+
+    }
+}
