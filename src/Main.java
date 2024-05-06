08c0e574919f9160806b3fdbf3f1c59ffc5af72d
Anders Stensland <43165915+andersstensland@users.noreply.github.com>
2024-05-06 13:14:31 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,1 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-
-
-    }
-
-
-
-
-}
